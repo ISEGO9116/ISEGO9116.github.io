3367b4c3eec49f54a09d3d8955610bbad28397d8
ISEGO9116 <161839212+ISEGO9116@users.noreply.github.com>
2024-05-16 00:44:45 +0900

    Delete js directory

--- js/splitbar.js
+++
@@ -1,9 +0,0 @@
-function dynamic_splitbar(){
-    let bar = document.querySelector('.splitbar');
-    bar.style.width = window.innerWidth - 200 + 'px';
-}
-window.onload = function() {
-    dynamic_splitbar();
-    //창 크기 조절로 인한 동적 사이즈 조절
-    window.addEventListener('resize', dynamic_splitbar);
-}
